5e97563305623b4f609e551c64cb3a09a3085410
ihorhladkov <ihor.hladkov@gmail.com>
2024-01-16 11:30:05 +0200

    initial commit

---
+++ src/models/post.ts
@@ -0,0 +1,14 @@
+import { BeforeInsert, Column, Entity, PrimaryColumn } from "typeorm";
+import { generateEntityId } from "@medusajs/utils";
+import { BaseEntity } from "@medusajs/medusa";
+
+@Entity()
+export class Post extends BaseEntity {
+  @Column({type: 'varchar'})
+  title: string | null;
+
+  @BeforeInsert()
+  private beforeInsert(): void {
+    this.id = generateEntityId(this.id, "post")
+  }
+}
